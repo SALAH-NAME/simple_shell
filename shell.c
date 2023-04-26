a01aed300960ed5de478988637c136cde0f966a4
SALAH-NAME <phone4ouahidi@gmail.com>
2023-04-26 17:25:56 +0000

    2.2 commit

--- shell.c
+++ shell.c
@@ -2,33 +2,35 @@
 
 /**
  * main - Entry point
+ * @argc: num of args
+ * @argv: array of args
  *
  * Return: Always 0
  */
 
-int main(void)
+int main(int __attribute__((unused)) argc, char **argv)
 {
 	char *line;
-	char *argv[2];
+	char *args[2];
 	int status, _run = 1;
 	size_t len = 0;
 
 	while (_run)
 	{
-		printf("$ ");
+		printf("#cisfun$ ");
 		line = NULL;
 
 		if (getline(&line, &len, stdin) == -1)
 			break;
 
 		line[strcspn(line, "\n")] = 0; /* remove newline character */
-		argv[0] = line;
-		argv[1] = NULL;
+		args[0] = line;
+		args[1] = NULL;
 
 		if (fork() == 0)
 		{
-			if (execve(argv[0], argv, NULL) == -1)
-				printf("./shell: No such file or directory\n");
+			if (execve(args[0], args, NULL) == -1)
+				printf("%s: No such file or directory\n", argv[0]);
 			exit(EXIT_FAILURE);
 		}
 		else
