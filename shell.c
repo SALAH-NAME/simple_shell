a97e9dd464830d7a6c2c86a08028f21c7216f8aa
SALAH-NAME <phone4ouahidi@gmail.com>
2023-04-26 17:11:39 +0000

    2 commit

---
+++ shell.c
@@ -0,0 +1,41 @@
+#include "shell.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char *line;
+	char *argv[2];
+	int status, _run = 1;
+	size_t len = 0;
+
+	while (_run)
+	{
+		printf("$ ");
+		line = NULL;
+
+		if (getline(&line, &len, stdin) == -1)
+			break;
+
+		line[strcspn(line, "\n")] = 0; /* remove newline character */
+		argv[0] = line;
+		argv[1] = NULL;
+
+		if (fork() == 0)
+		{
+			if (execve(argv[0], argv, NULL) == -1)
+				printf("%s: command not found\n", argv[0]);
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			wait(&status);
+		}
+	}
+
+	return (EXIT_SUCCESS);
+}
---
+++ shell.h
@@ -0,0 +1,11 @@
+#ifndef __SHELL__HEAD__
+#define __SHELL__HEAD__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+
+#endif
